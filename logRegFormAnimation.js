29c0c0018ac24f7d843c11c4e43dd5d26762c4d6
Miroslav Hornek <hornemir@gmail.com>
2024-01-08 22:12:44 +0100

    HTML návrh + CSS

---
+++ logRegFormAnimation.js
@@ -0,0 +1,25 @@
+const logButton = document.getElementById("logbtn")
+const regButton = document.getElementById("regbtn")
+const logForm = document.getElementById("logForm")
+const regForm = document.getElementById("regForm")
+
+function DisplayLog() {
+    if (regButton.style.display !== 'none') {
+        logForm.style.display = "none"
+        regForm.style.display = "flex"
+
+    } else {
+        logForm.style.display = "flex"
+        regForm.style.display = "none"
+    }
+}
+
+function DisplayReg() {
+    if (logButton.style.display !== 'none') {
+        logForm.style.display = "flex"
+        regForm.style.display = "none"
+    } else {        
+        logForm.style.display = "none"
+        regForm.style.display = "flex"
+    }
+}
