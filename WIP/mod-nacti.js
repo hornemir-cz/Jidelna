eb57b74b45adbab93953bffebe908d827e51b90d
Miroslav Hornek <hornemir@gmail.com>
2024-01-10 00:55:46 +0100

    WIP - Node, index edit content

---
+++ WIP/mod-nacti.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const util = require("util");
+
+const readFileMoje = util.promisify(fs.readFile);
+
+module.exports = function nactiStranku() {
+    console.log("nacitam stranku...");
+    return readFileMoje("main.html");
+    /*fs.readFile("main.html", function(err, data) {
+        if (err) {
+            console.log("Moc se omlouvám! Udělal jsem chybu: ", err);
+            throw err;
+        }
+        return data;
+    })*/
+}
