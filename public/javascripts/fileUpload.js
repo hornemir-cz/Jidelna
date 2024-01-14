21d9dc3160c2fcdeab5b8346971fa86db8454975
Miroslav Hornek <hornemir@gmail.com>
2024-01-14 17:21:22 +0100

    Zakomentovany fileUpload.js

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,13 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-  )
+// FilePond.registerPlugin(
+//     FilePondPluginImagePreview,
+//     FilePondPluginImageResize,
+//     FilePondPluginFileEncode,
+//   )
   
-  FilePond.setOptions({
-    stylePanelAspectRatio: 350 / 350,
-    imageResizeTargetWidth: 350,
-    imageResizeTargetHeight: 350
-  })
+//   FilePond.setOptions({
+//     stylePanelAspectRatio: 350 / 350,
+//     imageResizeTargetWidth: 350,
+//     imageResizeTargetHeight: 350
+//   })
   
-  FilePond.parse(document.body);
+//   FilePond.parse(document.body);
